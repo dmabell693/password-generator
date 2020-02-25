fa614eebfa5910e33d7c8434250f8fced5341439
Daniel Abell <da450@cairn.edu>
2020-02-24 20:39:17 -0500

    converted static variables that generate random characters into functions

--- assets/javascript.js
+++ assets/javascript.js
@@ -7,28 +7,37 @@ var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 // triggered by clicking "generate password" button in html doc
 function generatePassword() {
     
+    // confirm user preferences for password criteria
     var passLength = prompt("Your password must be between 8 and 128 characters. How long would you like your password to be? (please enter the number only, i.e., '8' or '22', etc.)");
-
     var passCharTypesLower = confirm("Your password must contain: lowercase, uppercase, numeric, and/or special characters. Would you like lowercase letters? ('OK' = yes; 'Cancel' = no)");
-
     var passCharTypesUpper = confirm("Would you like uppercase letters? ('OK' = yes; 'Cancel' = no)");
-
     var passCharTypesNum = confirm("Would you like numbers? ('OK' = yes; 'Cancel' = no)");
-
     var passCharTypesSpec = confirm("Would you like special characters? ('OK' = yes; 'Cancel' = no)");
 
     var passLengthNum = parseInt(passLength);
-    var randSpecCharArr = specCharArr[Math.floor(Math.random() * specCharArr.length)];
-    var randLowerArr = lowerArr[Math.floor(Math.random() * lowerArr.length)];
-    var randUpperArr = upperArr[Math.floor(Math.random() * upperArr.length)];
-    var randNum = Math.floor(Math.random() * 9);
+    var getRandSpecChar = function() {
+        return specCharArr[Math.floor(Math.random() * specCharArr.length)];
+    }
+    var getRandLowerArr = function() {
+        return lowerArr[Math.floor(Math.random() * lowerArr.length)];
+    }
+    var getRandUpperArr = function() {
+        return upperArr[Math.floor(Math.random() * upperArr.length)];
+    }
+    var getRandNum = function() {
+        Math.floor(Math.random() * 9);
+    }
 
+    
+
+
+    /*
     var optionsArr = [randSpecCharArr, randLowerArr, randUpperArr, randNum];
 
-    // function generateChar() {
-    //     return optionsArr[Math.floor(Math.random() * optionsArr.length)][Math.floor(Math.random() * randSpecCharArr.length)];
-    // }
-    // console.log(generateChar);
+    function generateChar() {
+        return optionsArr[Math.floor(Math.random() * optionsArr.length)];
+    }
+    console.log(generateChar);
     if (passCharTypesLower === true && passCharTypesUpper === true && passCharTypesNum === true && passCharTypesSpec === true) {
         alert("Your password is: " + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)]);
 
@@ -39,6 +48,4 @@ function generatePassword() {
 
 }
 
-console.log(specCharArr.length);
-console.log(lowerArr.length);
-console.log(upperArr.length);
+*/}
