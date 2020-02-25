ad55867f4189664ba81153bedce0cd75ead67d99
Daniel Abell <da450@cairn.edu>
2020-02-25 15:37:26 -0500

    scrapped plan to use arrays containing arrays of individual character types along with function that randomly drew from the former arrays. replaced with plan to create arrays accounting for each possibility of combinations of character types, then created function that draw from each array. moved old code in 'throwaway-js' file.

---
+++ assets/javascript-throwaway.js
@@ -0,0 +1,110 @@
+// create and store function in variable that randomly draws from functions above
+    // var optionsArr4 = [
+    //     getRandSpecChar(),
+    //     getRandLowerChar(),
+    //     getRandUpperChar(),
+    //     getRandNum(),
+    // ]
+    // function getOptionsArr4() {
+    //     return optionsArr4[Math.floor(Math.random() * optionsArr4.length)];
+    // }
+    // console.log(optionsArr4);
+
+
+
+    // function buildPassword() {
+    //     var myPassword = getOptionsArr4().concat(getOptionsArr4());
+    // }
+    // console.log(getOptionsArr4().concat(getOptionsArr4()));
+
+    // if (passCharTypesLower === true && passCharTypesUpper === true && passCharTypesNum === true && passCharTypesSpec === true) {
+    //     getOptionsArr4();
+    //         for (i = 0; i < passLengthNum; i++) {
+    //             console.log(getOptionsArr4());
+    //         }
+    //     }
+
+    // var optionsArr3a = [
+    //     getRandNum(),
+    //     getRandSpecChar(),
+    //     getRandUpperChar(),
+    // ]
+    // function getOptionsArr3a() {
+    //     return optionsArr3a[Math.floor(Math.random() * optionsArr3a.length)];
+    // }
+    // // console.log(optionsArr3a);
+
+    // var optionsArr3b = [
+    //     getRandNum(),
+    //     getRandLowerChar(),
+    //     getRandUpperChar(),
+    // ]
+    // function getOptionsArr3b() {
+    //     return optionsArr3b[Math.floor(Math.random() * optionsArr3b.length)];
+    // }
+    // // console.log(optionsArr3b);
+
+    // var optionsArr3c = [
+    //     getRandLowerChar(),
+    //     getRandSpecChar(),
+    //     getRandUpperChar(),
+    // ]
+    // function getOptionsArr3c() {
+    //     return optionsArr3c[Math.floor(Math.random() * optionsArr3c.length)];
+    // }
+    // // console.log(optionsArr3c);
+
+
+    // var optionsArr2a = [
+    //     getRandNum(),
+    //     getRandSpecChar(),
+    // ]
+    // function getOptionsArr2a() {
+    //     return optionsArr2a[Math.floor(Math.random() * optionsArr2a.length)];
+    // }
+    // // console.log(optionsArr2a);
+
+    // var optionsArr2b = [
+    //     getRandNum(),
+    //     getRandUpperChar(),
+    // ]
+    // function getOptionsArr2b() {
+    //     return optionsArr2b[Math.floor(Math.random() * optionsArr2b.length)];
+    // }
+    // // console.log(optionsArr2b);
+
+    // var optionsArr2c = [
+    //     getRandNum(),
+    //     getRandLowerChar(),
+    // ]
+    // function getOptionsArr2c() {
+    //     return optionsArr2c[Math.floor(Math.random() * optionsArr2c.length)];
+    // }
+    // // console.log(optionsArr2c);
+
+    // var optionsArr2d = [
+    //     getRandUpperChar(),
+    //     getRandSpecChar(),
+    // ]
+    // function getOptionsArr2d() {
+    //     return optionsArr2d[Math.floor(Math.random() * optionsArr2d.length)];
+    // }
+    // // console.log(optionsArr2d);
+
+    // var optionsArr2e = [
+    //     getRandLowerChar(),
+    //     getRandSpecChar(),
+    // ]
+    // function getOptionsArr2e() {
+    //     return optionsArr2e[Math.floor(Math.random() * optionsArr2e.length)];
+    // }
+    // // console.log(optionsArr2e);
+
+    // var optionsArr2f = [
+    //     getRandUpperChar(),
+    //     getRandLowerChar(),
+    // ]
+    // function getOptionsArr2f() {
+    //     return optionsArr2f[Math.floor(Math.random() * optionsArr2f.length)];
+    // }
+    // // console.log(optionsArr2f);
--- assets/javascript.js
+++ assets/javascript.js
@@ -2,50 +2,194 @@
 var specCharArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
 var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+var numSpecArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"]
+var numUpperArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numLowerArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specUpperArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var specLowerArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperLowerArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var numSpecUpperArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var numUpperLowerArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var specUpperLowerArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var numSpecUpperLowerArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 // triggered by clicking "generate password" button in html doc
 function generatePassword() {
     
-    // confirm user preferences for password criteria
+// confirm user preferences for password criteria
     var passLength = prompt("Your password must be between 8 and 128 characters. How long would you like your password to be? (please enter the number only, i.e., '8' or '22', etc.)");
+    // convert passLength into number
+    var passLengthNum = parseInt(passLength);
+    // account for possibility of illegitimate password length request
+    if (passLengthNum < 8 || passLengthNum > 128) {
+        alert("Please enter a number between 8 and 128");
+        generatePassword();
+    } else {
     var passCharTypesLower = confirm("Your password must contain: lowercase, uppercase, numeric, and/or special characters. Would you like lowercase letters? ('OK' = yes; 'Cancel' = no)");
     var passCharTypesUpper = confirm("Would you like uppercase letters? ('OK' = yes; 'Cancel' = no)");
     var passCharTypesNum = confirm("Would you like numbers? ('OK' = yes; 'Cancel' = no)");
     var passCharTypesSpec = confirm("Would you like special characters? ('OK' = yes; 'Cancel' = no)");
+    }
 
-    var passLengthNum = parseInt(passLength);
+
+
+// create and store functions in variables that randomly generate characters of one type
     var getRandSpecChar = function() {
         return specCharArr[Math.floor(Math.random() * specCharArr.length)];
     }
-    var getRandLowerArr = function() {
+    var getRandLowerChar = function() {
         return lowerArr[Math.floor(Math.random() * lowerArr.length)];
     }
-    var getRandUpperArr = function() {
+    var getRandUpperChar = function() {
         return upperArr[Math.floor(Math.random() * upperArr.length)];
     }
     var getRandNum = function() {
-        Math.floor(Math.random() * 9);
+        return numArr[Math.floor(Math.random() * 9)];
     }
 
-    
-
-
-    /*
-    var optionsArr = [randSpecCharArr, randLowerArr, randUpperArr, randNum];
-
-    function generateChar() {
-        return optionsArr[Math.floor(Math.random() * optionsArr.length)];
+// create and store functions in variables that randomly generate characters of two types
+    var getRandNumSpec = function() {
+        return numSpecArr[Math.floor(Math.random() * numSpecArr.length)];
     }
-    console.log(generateChar);
-    if (passCharTypesLower === true && passCharTypesUpper === true && passCharTypesNum === true && passCharTypesSpec === true) {
-        alert("Your password is: " + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)] + optionsArr[Math.floor(Math.random() * optionsArr.length)]);
-
-        // for (i = 0; i < passLengthNum; i++) {
-        //     generateChar;
-        // }
+    var getRandNumUpper = function() {
+        return numUpperArr[Math.floor(Math.random() * numUpperArr.length)];
+    }
+    var getRandNumLower = function() {
+        return numLowerArr[Math.floor(Math.random() * numLowerArr.length)];
+    }
+    var getRandSpecUpper = function() {
+        return specUpperArr[Math.floor(Math.random() * specUpperArr.length)];
+    }
+    var getRandSpecLower = function() {
+        return specLowerArr[Math.floor(Math.random() * specLowerArr.length)];
+    }
+    var getRandUpperLower = function() {
+        return upperLowerArr[Math.floor(Math.random() * upperLowerArr.length)];
     }
 
+// create and store functions in variables that randomly generate characters of three types
+    var getRandNumSpecUpper = function() {
+        return numSpecUpperArr[Math.floor(Math.random() * numSpecUpperArr.length)];
+    }
+    var getRandNumUpperLower = function() {
+        return numUpperLowerArr[Math.floor(Math.random() * numUpperLowerArr.length)];
+    }
+    var getRandSpecUpperLower = function() {
+        return specUpperLowerArr[Math.floor(Math.random() * specUpperLowerArr.length)];
+    }
+
+// create and store functions in variables that randomly generate characters of all four types
+    var getNumSpecUpperLowerArr = function() {
+        return numSpecUpperLowerArr[Math.floor(Math.random() * numSpecUpperLowerArr.length)]
+    }
+
+    var buildPassword = '';
+
+// create conditionals for:
+// all 4 character types
+    if (passCharTypesNum === true && passCharTypesSpec === true && passCharTypesUpper === true && passCharTypesLower === true) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getNumSpecUpperLowerArr();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+// 3 character types
+    if (passCharTypesSpec === true && passCharTypesUpper === true && passCharTypesLower === true && passCharTypesNum === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandSpecUpperLower();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesNum === true && passCharTypesUpper === true && passCharTypesLower === true && passCharTypesSpec === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandNumUpperLower();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesNum === true && passCharTypesSpec === true && passCharTypesUpper === true && passCharTypesLower === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandNumSpecUpper();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+// 2 character types
+    if (passCharTypesUpper === true && passCharTypesLower === true && passCharTypesSpec === false && passCharTypesNum === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandUpperLower();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesSpec === true && passCharTypesLower === true && passCharTypesUpper === false && passCharTypesNum === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandSpecLower();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesSpec === true && passCharTypesUpper === true && passCharTypesLower === false && passCharTypesNum === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandSpecUpper();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesNum === true && passCharTypesLower === true && passCharTypesUpper === false && passCharTypesSpec === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandNumLower();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesNum === true && passCharTypesUpper === true && passCharTypesLower === false && passCharTypesSpec === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandNumUpper();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesNum === true && passCharTypesSpec === true && passCharTypesLower === false && passCharTypesUpper === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandNumSpec();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+// 1 character type
+    if (passCharTypesLower === true && passCharTypesNum === false && passCharTypesSpec === false && passCharTypesUpper === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandLowerChar();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesUpper === true && passCharTypesNum === false && passCharTypesSpec === false && passCharTypesLower === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandUpperChar();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesSpec === true && passCharTypesNum === false && passCharTypesLower === false && passCharTypesUpper === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandSpecChar();
+        }
+        alert("Your password is: " + buildPassword);
+    }
+
+    if (passCharTypesNum === true && passCharTypesSpec === false && passCharTypesLower === false && passCharTypesUpper === false) {
+        for (i = 0; i < passLengthNum; i++) {
+            buildPassword += getRandNum();
+        }
+        alert("Your password is: " + buildPassword);
+    }
 }
-
-*/}
